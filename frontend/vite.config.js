4f223f9f0f38d53b5b955e928dfcb52fa42bcc53
MauroJircik <130071358+MauroJircik@users.noreply.github.com>
2025-06-17 13:38:08 -0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  define: {
+    'process.env': process.env
+  },
   server: {
     proxy: {
       '/users': 'http://localhost:3001',
