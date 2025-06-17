309c638bd7fec2aee514f3b57720e7c551dd2fed
MauroJircik <130071358+MauroJircik@users.noreply.github.com>
2025-06-17 14:43:49 -0300

    Update purchase.js

--- backend/models/purchase.js
+++ backend/models/purchase.js
@@ -10,14 +10,27 @@ export default (sequelize) => {
     userId: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      references: {
+        model: "Users",
+        key: "id",
+      },
+      onDelete: "CASCADE",
     },
     itemId: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      references: {
+        model: "Items",
+        key: "id",
+      },
+      onDelete: "CASCADE",      
     },
     quantity: {
       type: DataTypes.INTEGER,
       defaultValue: 1,
+      validate: {
+        min: 1, // quantidade mínima 1
+      },      
     },
   }, {
     timestamps: false,
